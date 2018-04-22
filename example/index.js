023fff802eb01a5e0aad95a905e162b7cac80409
Karl <karl.turrell@buto.tv>
2018-04-22 09:47:11 +0100

    Adjust example to show arbitrary dimensions

--- example/index.js
+++ example/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom'
 import Carousel3D from '../src/Carousel3D'
 
 ReactDOM.render(
-    <Carousel3D />,
+    <Carousel3D panelWidth={500} panelHeight={700} />,
     document.getElementById('root')
 )
 

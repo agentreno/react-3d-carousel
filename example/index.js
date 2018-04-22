cb1fabd05f7385a7240be508515b888ce3d9b12d
Karl <karl.turrell@buto.tv>
2018-04-22 10:12:47 +0100

    Demonstrate usage of arbitrary length and content for panels

--- example/index.js
+++ example/index.js
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom'
 
 import Carousel3D from '../src/Carousel3D'
 
+const Placeholder = () => (
+    <h2>Hello</h2>
+)
+
+const panels = Array(6).fill(Placeholder)
+
 ReactDOM.render(
-    <Carousel3D panelWidth={500} panelHeight={700} />,
+    <Carousel3D
+        panelWidth={500}
+        panelHeight={700}
+        panelRenderers={panels}
+    />,
     document.getElementById('root')
 )
 
